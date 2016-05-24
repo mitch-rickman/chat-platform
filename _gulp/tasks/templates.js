cce3351687ef6583d7364850e223eb754b0b1dea
mitch-rickman <mitchrickman@gmail.com>
2016-05-24 10:09:06 -0700

    restructure

--- _gulp/tasks/templates.js
+++
@@ -1,7 +0,0 @@
-var config = require('../gulp.config');
-var gulp = require('gulp');
-
-gulp.task('build:templates', function() {
-    return gulp.src( config.templates.src )
-        .pipe( gulp.dest(config.templates.build) );
-});
