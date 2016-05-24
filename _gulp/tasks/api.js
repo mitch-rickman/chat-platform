7fee69f3111e3a9bbdfe5778f7865e69b6a88e18
Mitch Rickman <mrickman@nextit.com>
2016-05-24 16:06:46 -0700

    restructure

--- _gulp/tasks/api.js
+++
@@ -1,9 +0,0 @@
-var config = require('../gulp.config');
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-
-gulp.task('build:api', function() {
-    return gulp.src( config.api.src )
-        .pipe( babel() )
-        .pipe( gulp.dest( config.api.build ) );
-});
